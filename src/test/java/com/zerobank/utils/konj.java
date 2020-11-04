95dfe3a828fc7b86578ac2da952d960710fcd965
Bumblebee88 <uros.andrijanic@gmail.com>
2020-11-04 15:28:16 -0600

    novi commit

--- src/test/java/com/zerobank/utils/konj.java
+++ src/test/java/com/zerobank/utils/konj.java
@@ -3,6 +3,9 @@ package com.zerobank.utils;
 public class konj {
     public static void main(String[] args) {
         //update horse
+        //idemo sada necemo veceras
+        //znas da sam uvek bio dobar momak
+
 
 
     }
