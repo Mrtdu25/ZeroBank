400769bf1e516835dc40804e3e19261c6cc68fd7
nikolaM500 <nikola.markovic2014@gmail.com>
2020-11-05 09:17:29 -0700

    login steps

---
+++ src/test/java/com/zerobank/step_definitions/Login_steps.java
@@ -0,0 +1,28 @@
+package com.zerobank.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Login_steps {
+
+    @Given("user is on the landing page")
+    public void user_is_on_the_landing_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user logs in")
+    public void user_logs_in() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user should see ZeroBank homepage")
+    public void user_should_see_zero_bank_homepage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+}
