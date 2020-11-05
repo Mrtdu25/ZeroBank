0bcea5458460dce7efba5e955941b574a641b2c5
Bumblebee88 <uros.andrijanic@gmail.com>
2020-11-05 12:09:11 -0600

    Assert commit

--- src/test/java/com/zerobank/runner/CukesRunner.java
+++ src/test/java/com/zerobank/runner/CukesRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/zerobank/step_definitions",
         tags = "@login",
-        dryRun = true,
+        dryRun = false,
         publish = false
 
 
