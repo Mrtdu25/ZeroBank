78a72f729137989eb2249d6fabe1f24fd02ef5e4
Murat <degirmenci_murat@yahoo.com>
2020-11-09 10:20:37 -0500

    Updated

--- src/test/java/com/zerobank/utils/Driver.java
+++ src/test/java/com/zerobank/utils/Driver.java
@@ -4,7 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
@@ -13,28 +13,26 @@ public class Driver {
 
     private static WebDriver driver;
 
-    public static WebDriver getDriver() {
+    public static WebDriver getDriver() throws MalformedURLException {
 
         if (driver==null){
 
             String browser = ConfigurationReader.getProperty("browser");
 
-            switch (browser){
+            switch (browser) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
-                    driver=new ChromeDriver();
+                    driver = new ChromeDriver();
                     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     driver.manage().window().maximize();
                     break;
 
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
-                    driver=new FirefoxDriver();
+                    driver = new FirefoxDriver();
                     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     driver.manage().window().maximize();
                     break;
-
-
             }
 
 
